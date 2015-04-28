fd5a3b26ca6dcddda9a111ece8af2015665b7e39
Stef van den Berg <vandenberg.stef@gmail.com>
2015-04-28 21:35:46 +0200

    Changed the default route to dashboard

--- resources/assets/src/app/dashboard/dashboard.route.js
+++ resources/assets/src/app/dashboard/dashboard.route.js
@@ -16,7 +16,7 @@
             {
                 state: 'dashboard',
                 config: {
-                    url: '/dashboard',
+                    url: '/',
                     templateUrl: 'app/dashboard/dashboard.html',
                     controller: 'DashboardController',
                     controllerAs: 'vm',
